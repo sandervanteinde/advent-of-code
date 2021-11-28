5d117f50a85a2bc30d14aa597d54fa0430337efb
Sander van 't Einde <sander.vanteinde@xprtz.net>
2021-11-28 21:23:49 +0100

    Day 14

---
+++ Sandervanteinde.AdventOfCode2021.Solutions/2015/Day14.ReindeerStats.cs
@@ -0,0 +1,33 @@
+﻿namespace Sandervanteinde.AdventOfCode2021.Solutions._2015;
+
+internal partial class Day14
+{
+    private class ReindeerStats
+    {
+        public string ReindeerName { get; init; }
+        public int SpeedInKms { get; init; }
+        public int FlyDurationInSeconds { get; init; }
+        public int RestTimeInSeconds { get; init; }
+        public int CycleTime { get; init; }
+
+        public override string ToString()
+        {
+            return $"{ReindeerName} can fly {SpeedInKms} km/s for {FlyDurationInSeconds} seconds, but then must rest for {RestTimeInSeconds} seconds.";
+        }
+
+        public int DistanceTravelledAfter(int seconds)
+        {
+
+            var positionInLastCycle = seconds % CycleTime;
+            var cyclesFlown = seconds / CycleTime;
+            var kmsFlownInCycles = cyclesFlown * FlyDurationInSeconds * SpeedInKms;
+
+            var secondsFlownInLastCycle =
+                positionInLastCycle > FlyDurationInSeconds
+                ? FlyDurationInSeconds
+                : positionInLastCycle;
+
+            return kmsFlownInCycles + secondsFlownInLastCycle * SpeedInKms;
+        }
+    }
+}
