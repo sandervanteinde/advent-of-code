542e74ec137a1022986876fba85261596a6988ee
Sander van 't Einde <sandervanteinde@gmail.com>
2021-12-04 15:26:19 +0100

    2015 - day 25

--- Sandervanteinde.AdventOfCode2021.Solutions/GlobalUsings.cs
+++ Sandervanteinde.AdventOfCode2021.Solutions/GlobalUsings.cs
@@ -1,1 +1,4 @@
 ﻿global using Sandervanteinde.AdventOfCode2021.Solutions.Utils;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Sandervanteinde.AdventOfCode2021.Solutions.Tests")]
