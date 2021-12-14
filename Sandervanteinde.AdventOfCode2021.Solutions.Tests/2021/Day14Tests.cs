22ec1234e0d28a2304876416e909348175aaec7a
Sander van 't Einde <sandervanteinde@gmail.com>
2021-12-14 08:43:45 +0100

    2021 day 14

---
+++ Sandervanteinde.AdventOfCode2021.Solutions.Tests/2021/Day14Tests.cs
@@ -0,0 +1,79 @@
+﻿using FluentAssertions;
+using Sandervanteinde.AdventOfCode2021.Solutions._2021;
+using Sandervanteinde.AdventOfCode2021.Solutions.Utils;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Sandervanteinde.AdventOfCode2021.Solutions.Tests._2021;
+
+public class Day14Tests
+{
+    private readonly FileReader _reader;
+    private readonly Day14 _sut;
+
+    public Day14Tests()
+    {
+        _reader = new FileReader(@"NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C");
+
+        _sut = new Day14();
+    }
+
+    [Fact]
+    public void StepOne_ShouldWorkWithExample()
+    {
+        _sut.DetermineStepOneResult(_reader)
+            .Should().Be(1588);
+    }
+
+    [Fact]
+    public void StepTwo_ShouldWorkWithExample()
+    {
+        _sut.DetermineStepTwoResult(_reader)
+            .Should().Be(2_188_189_693_529);
+    }
+
+    [Fact]
+    public void AddCountsToDictionary_ShouldWork()
+    {
+        var rule = new Dictionary<string, char>
+        {
+            { "AA", 'A' },
+            { "AB", 'C' },
+            { "AC", 'A' },
+            { "BA", 'B' },
+            { "BB", 'C' },
+            { "BC", 'A' },
+            { "CA", 'B' },
+            { "CB", 'C' },
+            { "CC", 'A' }
+        };
+        var key = new Day14.LookupKey
+        {
+            Key = "AB",
+            Index = 1
+        };
+        var result = _sut.AddCountsToDictionary(rule, new(), in key, 3);
+        result.Should().BeEquivalentTo(new Dictionary<char, ulong>
+        {
+            ['A'] = 4,
+            ['C'] = 3
+        });
+    }
+}
