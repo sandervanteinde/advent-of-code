e45a2b44b74c9793ef7866ac267bd85ad6c5a2b1
Sander van 't Einde <sander.vanteinde@xprtz.net>
2021-12-15 19:36:21 +0100

    2016 day 4

---
+++ Sandervanteinde.AdventOfCode2021.Solutions.Tests/2016/Day04Tests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using Sandervanteinde.AdventOfCode2021.Solutions._2016;
+using Xunit;
+
+namespace Sandervanteinde.AdventOfCode2021.Solutions.Tests._2016;
+
+public class Day04Tests
+{
+    [Theory]
+    [InlineData("aaaaa-bbb-z-y-x", 123, "abxyz", true)]
+    [InlineData("a-b-c-d-e-f-g-h", 987, "abcde", true)]
+    [InlineData("not-a-real-room", 404, "oarel", true)]
+    [InlineData("totally-real-room", 200, "decoy", false)]
+    public void Code_ValidatesChecksum(string name, int sectorId, string checkSum, bool isValid)
+    {
+        // arrange
+        var code = new Day04.Code
+        {
+            SectorId = sectorId,
+            Checksum = checkSum,
+            Name = name
+        };
+
+        // act
+        var result = code.IsValidChecksum();
+
+        // assert
+        result.Should().Be(isValid);
+    }
+
+    [Theory]
+    [InlineData("a", 1, "b")]
+    [InlineData("abc", 1, "bcd")]
+    [InlineData("abc", 26, "abc")]
+    [InlineData("abc", 27, "bcd")]
+    public void Decipher_ShouldCorrectlyDecipher(string name, int sectorId, string decipheredText)
+    {
+        // arrange
+        var code = new Day04.Code
+        {
+            Checksum = "",
+            Name = name,
+            SectorId = sectorId
+        };
+
+        // act
+        var result = code.Decipher();
+
+        // assert
+        result.Should().Be(decipheredText);
+    }
+}
---
+++ Sandervanteinde.AdventOfCode2021.Solutions/2016/Day04.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Sandervanteinde.AdventOfCode2021.Solutions._2016;
+
+internal partial class Day04 : BaseSolution
+{
+    public Day04()
+        : base("Security Through Obscurity", 2016, 4)
+    {
+
+    }
+    public override object DetermineStepOneResult(FileReader reader)
+    {
+        return ParseCodes(reader)
+            .Where(code => code.IsValidChecksum())
+            .Sum(code => code.SectorId);
+    }
+
+    public override object DetermineStepTwoResult(FileReader reader)
+    {
+        foreach (var cipher in ParseCodes(reader).Where(code => code.IsValidChecksum()))
+        {
+            var deciphered = cipher.Decipher();
+            if (deciphered == "northpole object storage")
+            {
+                return cipher.SectorId;
+            }
+        }
+        throw new InvalidOperationException("Code not found");
+    }
+
+    private static IEnumerable<Code> ParseCodes(FileReader reader)
+    {
+        var regex = new Regex(@"([a-z-]+)-(\d+)\[([a-z]+)\]");
+        foreach (var match in reader.MatchLineByLine(regex))
+        {
+            yield return new Code
+            {
+                Name = match.Groups[1].Value,
+                Checksum = match.Groups[3].Value,
+                SectorId = int.Parse(match.Groups[2].Value)
+            };
+        }
+    }
+}
