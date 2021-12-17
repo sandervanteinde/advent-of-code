0e688effb3d4fb90d62e474e780daed1f3f20510
Sander van 't Einde <sandervanteinde@gmail.com>
2021-12-17 08:06:25 +0100

    2021 day 17

---
+++ Sandervanteinde.AdventOfCode2021.Solutions/Utils/Area.cs
@@ -0,0 +1,26 @@
+﻿namespace Sandervanteinde.AdventOfCode2021.Solutions.Utils;
+
+internal class Area
+{
+    public Point TopLeft { get; init; }
+    public Point TopRight => new Point { X = BottomRight.X, Y = TopLeft.Y };
+    public Point BottomRight { get; init; }
+    public Point BottomLeft => new Point { X = TopLeft.X, Y = BottomRight.Y };
+
+    public bool IsInArea(Point p)
+    {
+        return p.X >= TopLeft.X
+            && p.X <= BottomRight.X
+            && p.Y >= TopLeft.Y
+            && p.Y <= BottomRight.Y;
+    }
+
+    public static Area FromTopLeftAndBottomRight(Point topLeft, Point bottomRight)
+    {
+        return new Area
+        {
+            TopLeft = topLeft,
+            BottomRight = bottomRight
+        };
+    }
+}
