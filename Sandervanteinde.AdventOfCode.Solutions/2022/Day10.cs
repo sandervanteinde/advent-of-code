3a7aa48909d27ce9b7fe71a480572a371d750373
Sander van 't Einde <github@sandervanteinde.nl>
2022-12-11 14:27:01 +0100

    2022 day 10

---
+++ Sandervanteinde.AdventOfCode.Solutions/2022/Day10.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandervanteinde.AdventOfCode.Solutions._2022;
+
+internal class Day10 : BaseSolution
+{
+    public Day10()
+        : base("Cathode-Ray Tube", 2022, 10)
+    {
+            
+    }
+
+    public override object DetermineStepOneResult(FileReader reader)
+    {
+        var cycle = 0;
+        var x = 1;
+        var sum = 0;
+        foreach(var line in reader.ReadAsSpanLineByLine())
+        {
+            _ = line switch
+            {
+                ['a', 'd', 'd', 'x', ' ', ..var amount] => AddX(int.Parse(amount)),
+                "noop" => Noop()
+            };
+        }
+
+        return sum;
+
+        bool Noop()
+        {
+            IncrementCycle();
+            return true;
+        }
+
+        bool AddX(int amount)
+        {
+            IncrementCycle();
+            IncrementCycle();
+            x += amount;
+            return true;
+        }
+
+        void IncrementCycle()
+        {
+            cycle++;
+            if((cycle - 20) % 40 == 0)
+            {
+                sum += cycle * x;
+            }
+        }
+    }
+
+    public override object DetermineStepTwoResult(FileReader reader)
+    {
+        var cycle = 0;
+        var x = 1;
+        var sb = new StringBuilder();
+        foreach (var line in reader.ReadAsSpanLineByLine())
+        {
+            _ = line switch
+            {
+                ['a', 'd', 'd', 'x', ' ', .. var amount] => AddX(int.Parse(amount)),
+                "noop" => Noop()
+            };
+        }
+
+        return sb.ToString();
+
+        bool Noop()
+        {
+            IncrementCycle();
+            return true;
+        }
+
+        bool AddX(int amount)
+        {
+            IncrementCycle();
+            IncrementCycle();
+            x += amount;
+            return true;
+        }
+
+        void IncrementCycle()
+        {
+            var currentPosition = cycle % 40;
+            if(Math.Abs(currentPosition - x) <= 1)
+            {
+                sb.Append('#');
+            }
+            else
+            {
+                sb.Append('.');
+            }
+            cycle++;
+            if (cycle % 40 == 0)
+            {
+                sb.AppendLine();
+            }
+        }
+    }
+}
