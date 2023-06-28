fb868c2f44cdef10b55b917f7c544d4a9d9a19cb
Sander van 't Einde <github@sandervanteinde.nl>
2023-06-28 19:21:47 +0200

    Problem 6

---
+++ Sandervanteinde.AdventOfCode.Solutions/ProjectEuler/Problem0006.cs
@@ -0,0 +1,20 @@
+﻿namespace Sandervanteinde.AdventOfCode.Solutions.ProjectEuler;
+
+public class Problem0006 : IProjectEulerSolution
+{
+    public string Title => "Sum Square Difference";
+    public int Id => 6;
+    public object GetSolution()
+    {
+        var sumOfPowers = 0.0;
+        var sum = 0;
+        for (var i = 1; i <= 100; i++)
+        {
+            sumOfPowers += Math.Pow(i, 2);
+            sum += i;
+        }
+
+        var sumSquared = Math.Pow(sum, 2);
+        return sumSquared - sumOfPowers;
+    }
+}
