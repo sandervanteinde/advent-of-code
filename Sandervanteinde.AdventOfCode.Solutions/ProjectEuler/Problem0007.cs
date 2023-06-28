5b7d5420943c4a29d7495c96a26e567c5c4126b1
Sander van 't Einde <github@sandervanteinde.nl>
2023-06-28 19:24:25 +0200

    Problem 7

---
+++ Sandervanteinde.AdventOfCode.Solutions/ProjectEuler/Problem0007.cs
@@ -0,0 +1,13 @@
+﻿namespace Sandervanteinde.AdventOfCode.Solutions.ProjectEuler;
+
+public class Problem0007 : IProjectEulerSolution
+{
+    public string Title => "10001st Prime";
+    public int Id => 7;
+    public object GetSolution()
+    {
+        return Primes.Enumerate(100_000_000)
+           .Skip(10_000)
+           .First();
+    }
+}
